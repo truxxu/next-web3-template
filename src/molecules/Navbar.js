779942f072ab86ac0fcefb46738edb92c795dcec
truxxu <danielfetrujillo@gmail.com>
2022-08-29 06:58:18 -0500

    created price hook

--- src/molecules/Navbar.js
+++ src/molecules/Navbar.js
@@ -5,7 +5,7 @@ const Navbar = () => {
     <section>
       <div className="relative pt-6 px-4 sm:px-6 lg:px-8">
         <nav className="relative" aria-label="Global">
-          <div className="flex flex-col xs:flex-row justify-between items-center">
+          <div className="flex flex-col xs:flex-row">
             <div>
               <a className="font-medium mr-8 text-gray-500 hover:text-gray-900">
                 Home
