0721f36d5264ba29c934b47cdf167900f2b2a315
truxxu <danielfetrujillo@gmail.com>
2022-08-29 06:08:25 -0500

    added navbar and footer

--- src/molecules/Footer.js
+++ src/molecules/Footer.js
@@ -1,7 +1,19 @@
 import React from "react";
 
 const Footer = () => {
-  return <div>Footer</div>;
+  return (
+    <footer className="bg-indigo-400">
+      <div className="container mx-auto px-6">
+        <div className="flex flex-col items-center">
+          <div className="py-6">
+            <p className="text-white text-sm text-primary-2 font-bold">
+              © {new Date().getFullYear()} Truxxu
+            </p>
+          </div>
+        </div>
+      </div>
+    </footer>
+  );
 };
 
 export { Footer };
--- src/molecules/Navbar.js
+++ src/molecules/Navbar.js
@@ -1,7 +1,27 @@
 import React from "react";
 
 const Navbar = () => {
-  return <div>Navbar</div>;
+  return (
+    <section>
+      <div className="relative pt-6 px-4 sm:px-6 lg:px-8">
+        <nav className="relative" aria-label="Global">
+          <div className="flex flex-col xs:flex-row justify-between items-center">
+            <div>
+              <a className="font-medium mr-8 text-gray-500 hover:text-gray-900">
+                Home
+              </a>
+              <a className="font-medium mr-8 text-gray-500 hover:text-gray-900">
+                Store
+              </a>
+              <a className="font-medium mr-8 text-gray-500 hover:text-gray-900">
+                Blog
+              </a>
+            </div>
+          </div>
+        </nav>
+      </div>
+    </section>
+  );
 };
 
 export { Navbar };
