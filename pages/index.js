d6c4697969326e221d05628096113a5a07756a74
truxxu <danielfetrujillo@gmail.com>
2022-08-29 05:15:18 -0500

    installed tailwind css

--- pages/index.js
+++ pages/index.js
@@ -11,7 +11,7 @@ export default function Home() {
       </Head>
 
       <main>
-        <p>Test</p>
+        <h1 className="text-3xl font-bold underline">Hello world!</h1>
       </main>
     </div>
   );
